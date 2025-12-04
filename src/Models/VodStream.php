2341ac2f225a6255f5f7a9c26ddda9b4bc011c22
anasoid <oufdou.anas@gmail.com>
2025-12-04 21:47:20 +0100

    fix test &stan

--- src/Models/VodStream.php
+++ src/Models/VodStream.php
@@ -10,6 +10,18 @@ use RuntimeException;
  * VodStream Model
  *
  * Represents video-on-demand content
+ *
+ * @property int $id
+ * @property int $source_id
+ * @property int|string $stream_id
+ * @property string $name
+ * @property string|null $category_id
+ * @property string|null $category_ids
+ * @property int $is_adult
+ * @property string|null $labels
+ * @property string|null $data
+ * @property string $created_at
+ * @property string $updated_at
  */
 class VodStream extends BaseStreamModel
 {
