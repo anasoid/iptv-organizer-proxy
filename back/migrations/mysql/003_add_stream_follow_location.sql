5d249beb614597678fae048d55abf4403aa5fe14
anasoid <oufdou.anas@gmail.com>
2026-01-01 18:34:27 +0100

    alter sql

--- back/migrations/mysql/003_add_stream_follow_location.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add stream_follow_location to sources table
--- Description: Adds per-source configuration for following HTTP redirects when streaming
-
-ALTER TABLE sources
-ADD COLUMN stream_follow_location TINYINT(1) NOT NULL DEFAULT 1 COMMENT 'Follow HTTP redirects when streaming from this source';
