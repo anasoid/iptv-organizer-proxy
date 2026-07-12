4e729834b2ae17aab066a18f84246ef075f67614
anasoid <oufdou.anas@gmail.com>
2026-07-12 11:21:46 +0100

    xmml tv produce

--- back/src/main/java/org/anasoid/iptvorganizer/controllers/xtream/XmltvController.java
+++ back/src/main/java/org/anasoid/iptvorganizer/controllers/xtream/XmltvController.java
@@ -4,7 +4,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.*;
 import lombok.extern.slf4j.Slf4j;
